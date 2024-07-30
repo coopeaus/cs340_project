59c020918ba778f7e1081315b8a471d40f0a98b3
coopeaus <coopeaus@oregonstate.edu>
2024-07-29 20:02:59 -0700

    created wsgi.py, wrapped each route in a try-except-finally block for error handling. Open and close the db connection and cursor on each route due to instability.

---
+++ Group20_DDL.sql
@@ -0,0 +1,225 @@
+--
+-- Group: 20
+-- Team members: 
+-- Austin Cooper, Team Leader, Researcher, and Deadline Enforcer
+-- Wei-Yin Chen, Creativity Officer, Ed Discussion liaison and Submission Proofreader
+--
+
+--
+-- Database: `Hogwarts_Course_Registration_System`
+--
+
+-- --------------------------------------------------------
+
+SET FOREIGN_KEY_CHECKS=0;
+SET AUTOCOMMIT = 0;
+
+--
+-- Table structure for table `Students`
+--
+
+CREATE OR REPLACE TABLE Students (
+    student_id int(11) NOT NULL UNIQUE AUTO_INCREMENT,
+    house_id int(11) NOT NULL,
+    first_name varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL,
+    level_attending int(11) NOT NULL,
+    PRIMARY KEY (student_id),
+    FOREIGN KEY (house_id) REFERENCES Houses(house_id) ON DELETE CASCADE
+);
+
+--
+-- Table structure for table `Professors`
+--
+
+CREATE OR REPLACE TABLE Professors (
+    professor_id int(11) NOT NULL UNIQUE AUTO_INCREMENT,
+    first_name varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL,
+    PRIMARY KEY (professor_id)
+);
+
+--
+-- Table structure for table `Houses`
+--
+
+CREATE OR REPLACE TABLE Houses (
+    house_id int(11) NOT NULL UNIQUE AUTO_INCREMENT,
+    head_of_house int(11) NOT NULL,
+    house_name varchar(255) NOT NULL,
+    house_animal varchar(255) NOT NULL,
+    house_colors varchar(255) NOT NULL,
+    PRIMARY KEY (house_id),
+    FOREIGN KEY (head_of_house) REFERENCES Professors(professor_id) ON DELETE CASCADE
+);
+
+--
+-- Table structure for table `Subjects`
+--
+
+CREATE OR REPLACE TABLE Subjects (
+    subject_id int(11) NOT NULL UNIQUE AUTO_INCREMENT,
+    subject_name varchar(255) NOT NULL,
+    core_elective int(11) NOT NULL,
+    PRIMARY KEY (subject_id)
+);
+
+--
+-- Table structure for table `Classes`
+--
+
+CREATE OR REPLACE TABLE Classes (
+    class_id int(11) NOT NULL UNIQUE AUTO_INCREMENT,
+    subject_id int(11) NOT NULL,
+    professor_id int(11) NOT NULL,
+    class_level int(11) NOT NULL,
+    PRIMARY KEY (class_id),
+    FOREIGN KEY (subject_id) REFERENCES Subjects(subject_id) ON DELETE CASCADE,
+    FOREIGN KEY (professor_id) REFERENCES Professors(professor_id) ON DELETE CASCADE
+);
+
+--
+-- Table structure for table `Class_Registrations`
+--
+
+CREATE OR REPLACE TABLE Class_Registrations (
+    student_id int(11) NOT NULL,
+    class_id int(11) NOT NULL,
+    PRIMARY KEY (student_id, class_id),
+    FOREIGN KEY (student_id) REFERENCES Students(student_id) ON DELETE CASCADE,
+    FOREIGN KEY (class_id) REFERENCES Classes(class_id) ON DELETE CASCADE
+);
+
+--
+-- Dumping data for table `Professors`
+--
+
+INSERT INTO Professors (first_name, last_name)
+VALUES
+('Minerva', 'McGonagall'),
+('Pomona', 'Sprout'),
+('Filius', 'Flitwick'),
+('Severus', 'Snape');
+
+--
+-- Dumping data for table `Houses`
+--
+
+INSERT INTO Houses (head_of_house, house_name, house_animal, house_colors)
+VALUES
+((SELECT professor_id FROM Professors WHERE first_name = 'Minerva' AND last_name = 'McGonagall'), 'Gryffindor', 'Lion', 'Scarlet and gold'),
+((SELECT professor_id FROM Professors WHERE first_name = 'Pomona' AND last_name = 'Sprout'), 'Hufflepuff', 'Badger', 'Yellow and black'),
+((SELECT professor_id FROM Professors WHERE first_name = 'Filius' AND last_name = 'Flitwick'), 'Ravenclaw', 'Eagle', 'Blue and bronze'),
+((SELECT professor_id FROM Professors WHERE first_name = 'Severus' AND last_name = 'Snape'), 'Slytherin', 'Snake', 'Green and silver');
+
+--
+-- Dumping data for table `Subjects`
+--
+
+INSERT INTO Subjects (subject_name, core_elective)
+VALUES
+('Transfiguration', 1),
+('Defense Against the Dark Arts', 1),
+('Charms', 1), 
+('Potions', 1),
+('Astronomy', 1),
+('History of Magic', 1),
+('Herbology', 1),
+('Arithmancy', 0),
+('Muggle Studies', 0),
+('Divination', 0),
+('Study of Ancient Runes', 0),
+('Care of Magical Creatures', 0);
+
+--
+-- Dumping data for table `Students`
+--
+
+INSERT INTO Students (house_id, first_name, last_name, level_attending)
+VALUES
+((SELECT house_id FROM Houses WHERE house_name = 'Gryffindor'), 'Hermione', 'Granger', 5),
+((SELECT house_id FROM Houses WHERE house_name = 'Gryffindor'), 'Neville', 'Longbottom', 5),
+((SELECT house_id FROM Houses WHERE house_name = 'Gryffindor'), 'Harry', 'Potter', 5),
+((SELECT house_id FROM Houses WHERE house_name = 'Gryffindor'), 'Fred', 'Weasley', 7),
+((SELECT house_id FROM Houses WHERE house_name = 'Gryffindor'), 'George', 'Weasley', 7),
+((SELECT house_id FROM Houses WHERE house_name = 'Gryffindor'), 'Ginny', 'Weasley', 4),
+((SELECT house_id FROM Houses WHERE house_name = 'Gryffindor'), 'Ron', 'Weasley', 5),
+((SELECT house_id FROM Houses WHERE house_name = 'Hufflepuff'), 'Cedric', 'Diggory', 7),
+((SELECT house_id FROM Houses WHERE house_name = 'Hufflepuff'), 'Hannah', 'Abbott', 5),
+((SELECT house_id FROM Houses WHERE house_name = 'Ravenclaw'), 'Cho', 'Chang', 6),
+((SELECT house_id FROM Houses WHERE house_name = 'Ravenclaw'), 'Luna', 'Lovegood', 4),
+((SELECT house_id FROM Houses WHERE house_name = 'Slytherin'), 'Draco', 'Malfoy', 5),
+((SELECT house_id FROM Houses WHERE house_name = 'Slytherin'), 'Vincent', 'Crabbe', 5);
+
+--
+-- Dumping data for table `Classes`
+--
+
+INSERT INTO Classes (subject_id, professor_id, class_level)
+VALUES
+(1, 1, 1),
+(1, 1, 2),
+(1, 1, 3),
+(1, 1, 4),
+(1, 1, 5),
+(7, 2, 1),
+(7, 2, 2),
+(7, 2, 3),
+(7, 2, 4),
+(7, 2, 5),
+(3, 3, 1),
+(3, 3, 2),
+(3, 3, 3),
+(3, 3, 4),
+(3, 3, 5),
+(4, 4, 1),
+(4, 4, 2),
+(4, 4, 3),
+(4, 4, 4),
+(4, 4, 5);
+
+--
+-- Dumping data for table `Class_Registration`
+--
+
+INSERT INTO Class_Registrations (student_id, class_id)
+VALUES
+(1, 5),
+(1, 10),
+(1, 15),
+(1, 20),
+(2, 5),
+(2, 10),
+(2, 15),
+(2, 20),
+(3, 5),
+(3, 10),
+(3, 15),
+(3, 20),
+(7, 5),
+(7, 10),
+(7, 15),
+(7, 20),
+(9, 5),
+(9, 10),
+(9, 15),
+(9, 20),
+(12, 5),
+(12, 10),
+(12, 15),
+(12, 20),
+(13, 5),
+(13, 10),
+(13, 15),
+(13, 20),
+(6, 4),
+(6, 9),
+(6, 14),
+(6, 19),
+(11, 4),
+(11, 9),
+(11, 14),
+(11, 19);
+
+SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 from flask_wtf import CSRFProtect
+import MySQLdb
 import secrets
 import custom_forms
 import database.db_connector as db
@@ -18,7 +19,6 @@ app.secret_key = token
 
 bootstrap = Bootstrap5(app)
 csrf = CSRFProtect(app)
-db_connection = db.connect_to_database()
 
 
 # Routes
@@ -26,105 +26,163 @@ db_connection = db.connect_to_database()
 @app.route("/index.html")
 @app.route("/home")
 def home():
+    """Home page route"""
     return render_template("index.j2")
 
 
 @app.route("/students")
 def students():
-    query = "SELECT * FROM Students;"
-    cursor = db.execute_query(db_connection=db_connection, query=query)
-    students = cursor.fetchall()
-
-    # Structured these into a dictionary, to pass in as **kwargs
-    values = {
-        "title": "Students",
-        "records": students,
-        "enroll_form": custom_forms.NewStudentForm(),
-        "find_form": custom_forms.LookupStudentForm(),
-        "update_form": custom_forms.UpdateStudentForm(),
-        "delete_form": custom_forms.DeleteStudentForm(),
-    }
-    return render_template("students.j2", **values)
+    """Displays the Students page and associated CRUD operations"""
+    try:
+        # We were having connection issues, so decided
+        # to renew the DB connection on each request.
+        # Per the MySQLdb library docs, the cursor should be closed
+        # at a minimum each time.
+        db_connection = db.connect_to_database()
+        query = "SELECT * FROM Students;"
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        students = cursor.fetchall()
+        # Structured these into a dictionary, to pass in as **kwargs
+        values = {
+            "title": "Students",
+            "records": students,
+            "enroll_form": custom_forms.NewStudentForm(),
+            "find_form": custom_forms.LookupStudentForm(),
+            "update_form": custom_forms.UpdateStudentForm(),
+            "delete_form": custom_forms.DeleteStudentForm(),
+        }
+        return render_template("students.j2", **values)
+    except MySQLdb.Error as e:
+        # Catch and display any DB errors
+        return e
+    finally:
+        # Close the cursor and connection each time.
+        cursor.close()
+        db_connection.close()
 
 
 @app.route("/professors")
 def professors():
-    query = "SELECT * FROM Professors;"
-    cursor = db.execute_query(db_connection=db_connection, query=query)
-    professors = cursor.fetchall()
-    values = {
-        "title": "Professors",
-        "records": professors,
-        "new_prof": custom_forms.NewProfessorForm(),
-        "update_form": custom_forms.UpdateProfessorForm(),
-        "delete_form": custom_forms.DeleteProfessorForm(),
-    }
-    return render_template("professors.j2", **values)
+    """Displays the Professors page and associated CRUD operations"""
+    try:
+        db_connection = db.connect_to_database()
+        query = "SELECT * FROM Professors;"
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        professors = cursor.fetchall()
+        values = {
+            "title": "Professors",
+            "records": professors,
+            "new_prof": custom_forms.NewProfessorForm(),
+            "update_form": custom_forms.UpdateProfessorForm(),
+            "delete_form": custom_forms.DeleteProfessorForm(),
+        }
+        return render_template("professors.j2", **values)
+    except MySQLdb.Error as e:
+        # Catch and display any DB errors
+        return e
+    finally:
+        # Close the cursor and connection.
+        cursor.close()
+        db_connection.close()
 
 
 @app.route("/houses")
 def houses():
-    query = "SELECT * FROM Houses;"
-    cursor = db.execute_query(db_connection=db_connection, query=query)
-    houses = cursor.fetchall()
-    values = {
-        "title": "Houses",
-        "records": houses,
-        "new_house": custom_forms.NewHouseForm(),
-        "update_form": custom_forms.UpdateHouseForm(),
-        "delete_form": custom_forms.DeleteHouseForm(),
-    }
-    return render_template("houses.j2", **values)
+    """Displays the Houses page and associated CRUD operations"""
+    try:
+        db_connection = db.connect_to_database()
+        query = "SELECT * FROM Houses;"
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        houses = cursor.fetchall()
+        values = {
+            "title": "Houses",
+            "records": houses,
+            "new_house": custom_forms.NewHouseForm(),
+            "update_form": custom_forms.UpdateHouseForm(),
+            "delete_form": custom_forms.DeleteHouseForm(),
+        }
+        return render_template("houses.j2", **values)
+    except MySQLdb.Error as e:
+        # Catch and display any DB errors
+        return e
+    finally:
+        # Close the cursor and connection.
+        cursor.close()
+        db_connection.close()
 
 
 @app.route("/subjects")
 def subjects():
-    query = "SELECT * FROM Subjects;"
-    cursor = db.execute_query(db_connection=db_connection, query=query)
-    subjects = cursor.fetchall()
-    values = {
-        "title": "Subjects",
-        "records": subjects,
-        "new_sub": custom_forms.NewSubjectForm(),
-        "update_form": custom_forms.UpdateSubjectForm(),
-        "delete_form": custom_forms.DeleteSubjectForm(),
-    }
-    return render_template("subjects.j2", **values)
+    """Displays the Subjects page and associated CRUD operations"""
+    try:
+        db_connection = db.connect_to_database()
+        query = "SELECT * FROM Subjects;"
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        subjects = cursor.fetchall()
+        values = {
+            "title": "Subjects",
+            "records": subjects,
+            "new_sub": custom_forms.NewSubjectForm(),
+            "update_form": custom_forms.UpdateSubjectForm(),
+            "delete_form": custom_forms.DeleteSubjectForm(),
+        }
+        return render_template("subjects.j2", **values)
+    except MySQLdb.Error as e:
+        # Catch and display any DB errors
+        return e
+    finally:
+        # Close the cursor and connection.
+        cursor.close()
+        db_connection.close()
 
 
 @app.route("/classes")
 def classes():
-    query = "SELECT * FROM Classes;"
-    cursor = db.execute_query(db_connection=db_connection, query=query)
-    classes = cursor.fetchall()
-    values = {
-        "title": "Classes",
-        "records": classes,
-        "new_class": custom_forms.NewClassForm(),
-        "update_form": custom_forms.UpdateClassForm(),
-        "delete_form": custom_forms.DeleteClassForm(),
-    }
-    return render_template("classes.j2", **values)
+    """Displays the Classes page and associated CRUD operations"""
+    try:
+        db_connection = db.connect_to_database()
+        query = "SELECT * FROM Classes;"
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        classes = cursor.fetchall()
+        values = {
+            "title": "Classes",
+            "records": classes,
+            "new_class": custom_forms.NewClassForm(),
+            "update_form": custom_forms.UpdateClassForm(),
+            "delete_form": custom_forms.DeleteClassForm(),
+        }
+        return render_template("classes.j2", **values)
+    except MySQLdb.Error as e:
+        # Catch and display any DB errors
+        return e
+    finally:
+        cursor.close()
+        db_connection.close()
 
 
 @app.route("/registrations")
 def registrations():
-    query = "SELECT * FROM Class_Registrations;"
-    cursor = db.execute_query(db_connection=db_connection, query=query)
-    registrations = cursor.fetchall()
-    values = {
-        "title": "Registrations",
-        "records": registrations,
-        "buttons": [
-            "Find Students in a Class",
-            "Find Class Registrations for a Student",
-        ],
-        "new_reg": custom_forms.NewRegistrationForm(),
-        "delete_form": custom_forms.DeleteRegistrationForm(),
-    }
-    return render_template("registrations.j2", **values)
-
-
-# Listener
-if __name__ == "__main__":
-    app.run(port=65000, debug=True)
+    """Displays the Class_Registrations page and associated CRUD operations"""
+    try:
+        db_connection = db.connect_to_database()
+        query = "SELECT * FROM Class_Registrations;"
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        registrations = cursor.fetchall()
+        values = {
+            "title": "Registrations",
+            "records": registrations,
+            "buttons": [
+                "Find Students in a Class",
+                "Find Class Registrations for a Student",
+            ],
+            "new_reg": custom_forms.NewRegistrationForm(),
+            "delete_form": custom_forms.DeleteRegistrationForm(),
+        }
+        return render_template("registrations.j2", **values)
+    except MySQLdb.Error as e:
+        # Catch and display any DB errors
+        return e
+    finally:
+        # Close the cursor and connection.
+        cursor.close()
+        db_connection.close()
